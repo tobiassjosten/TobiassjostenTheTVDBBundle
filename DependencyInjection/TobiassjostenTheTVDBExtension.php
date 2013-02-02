5b39c21b121910a5acd4da12aff1df5f98dd19d0
Tobias Sjösten <tobias.sjosten@gmail.com>
2013-02-02 18:06:44 +0100

    Updated copyright email address

--- DependencyInjection/TobiassjostenTheTVDBExtension.php
+++ DependencyInjection/TobiassjostenTheTVDBExtension.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the TheTVDBBundle package.
  *
- * (c) Tobias Sjösten <tobias.sjosten@gmail.com>
+ * (c) Tobias Sjösten <tobias@tobiassjosten.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
